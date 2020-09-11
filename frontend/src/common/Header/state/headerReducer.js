4a497cf5fce22d06db65aff5665caa3c934113cf
Ranjan <Rkastew@gmail.com>
2020-09-11 10:02:55 +0545

    first commit

--- frontend/src/common/Header/state/headerReducer.js
+++
@@ -1,9 +0,0 @@
-const initiaState = 0;
-
-const headerReducer = (state = initiaState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
-export default headerReducer;
